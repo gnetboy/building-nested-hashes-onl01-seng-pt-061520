a59b64b17ffc46dacb32306437106a3ceb83a18e
Gustavo Hernandez <gnetboy.y@gmail.com>
2020-06-25 05:43:41 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -28,7 +28,7 @@ def third_challenge
       :heroine => {
         :name =>"Juliet",
         :age =>"15",
-        :status =>"alivel"
+        :status =>"alive"
         },
       :heroine_friends => []
    }
